6c7b44f600974eb292d032e0445286e556616a48
Mariia <maria8zhuk@gmail.com>
2024-09-19 20:48:35 +0200

    dynamic rote, error, loading pages

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,21 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
+export const dynamicParamd = true;
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
 async function getTicket(id) {
-  const res = await fetch("http://localhost:4000/tickets/" + id, {
-    next: { revalidate: 60 },
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: { revalidate: 30 },
   });
+  if (!res.ok) {
+    notFound();
+  }
   return res.json();
 }
 
